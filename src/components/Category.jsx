516bed35a8f4b2200638e5df0779d9055534865e
mikhailir <saburov1985@gmail.com>
2023-10-24 21:38:58 +1100

    Вынес в отдельные комопненты  header, category и sort

---
+++ src/components/Category.jsx
@@ -0,0 +1,14 @@
+export const Category = () => {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+};
