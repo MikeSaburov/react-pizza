dc26bc2ad11f38829fb393c5f9cec8271733a5e4
mikhailir <saburov1985@gmail.com>
2023-10-25 21:57:31 +1100

    Переименовал классы и атрибуты

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -8,28 +8,28 @@ export const PizzaBlock = () => {
   };
 
   return (
-    <div class="pizza-block">
+    <div className="pizza-block">
       <img
-        class="pizza-block__image"
+        className="pizza-block__image"
         src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
         alt="Pizza"
       />
-      <h4 class="pizza-block__title">Чизбургер-пицца</h4>
-      <div class="pizza-block__selector">
+      <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+      <div className="pizza-block__selector">
         <ul>
-          <li class="active">тонкое</li>
+          <li className="active">тонкое</li>
           <li>традиционное</li>
         </ul>
         <ul>
-          <li class="active">26 см.</li>
+          <li className="active">26 см.</li>
           <li>30 см.</li>
           <li>40 см.</li>
         </ul>
       </div>
-      <div class="pizza-block__bottom">
-        <div class="pizza-block__price">от 395 ₽</div>
-        <div
-          class="button button--outline button--add"
+      <div className="pizza-block__bottom">
+        <div className="pizza-block__price">от 395 ₽</div>
+        <button
+          className="button button--outline button--add"
           onClick={() => addPizzaCount()}
         >
           <svg
@@ -46,7 +46,7 @@ export const PizzaBlock = () => {
           </svg>
           <span>Добавить</span>
           <i>{count}</i>
-        </div>
+        </button>
       </div>
     </div>
   );
