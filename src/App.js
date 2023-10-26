c9827ffec0549f97d4aa635566e6b90c651a9efc
mikhailir <saburov1985@gmail.com>
2023-10-26 21:59:01 +1100

    Добавил json  с пиццами

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Category } from './components/Category';
 import { Sort } from './components/Sort';
 import { PizzaBlock } from './components/PizzaBlock';
 
+import pizzas from './assets/pizza.json';
+
 function App() {
   return (
     <div className="wrapper">
@@ -16,8 +18,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock />
-            <PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock />
+            ))}
           </div>
         </div>
       </div>
