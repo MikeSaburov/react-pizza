6f8d9274789243c85f041c9c1d2fc64a18e02762
mikhailir <saburov1985@gmail.com>
2023-10-26 22:55:43 +1100

    Упростил код, удалил функции для категории и передал в onClick сразу set...(id)

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -12,17 +12,13 @@ export const Category = () => {
     'Закрытые',
   ];
 
-  const categoryHandleClick = (index) => {
-    setActiveIndex(index);
-  };
-
   return (
     <div className="categories">
       <ul>
         {categories.map((category, index) => (
           <li
             key={index}
-            onClick={() => categoryHandleClick(index)}
+            onClick={() => setActiveIndex(index)}
             className={activeIndex === index ? 'active' : ''}
           >
             {category}
