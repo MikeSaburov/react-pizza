ee2ab31b9ef9a71ff8e4394a9a234bca2543f78c
Mikhail Saburov <saburov1985@gmail.com>
2023-10-29 23:12:51 +1100

    refactor(NotFoundBlock): заменен эмодзи

---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NotFoundBlock = () => {
+  return (
+    <div>
+      <h1>
+        <span>🙁</span>
+        <br />
+        Ничего не найдено{' '}
+      </h1>
+    </div>
+  );
+};
