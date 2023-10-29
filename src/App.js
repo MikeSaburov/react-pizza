373564f27904f6c00b1e48b4ada9f54e5e7a8af6
Mikhail Saburov <saburov1985@gmail.com>
2023-10-29 22:55:19 +1100

    feature(NotFound): Создана страница и импортир.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './scss/app.scss';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotFound = () => {
+  return <h1>Ничего не найдено :(</h1>;
+};
