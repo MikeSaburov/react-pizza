abff185e5e8509f3e95f4e00b78d5eb3be3105d2
Mikhail Saburov <saburov1985@gmail.com>
2023-10-30 20:29:56 +1100

    feature(App): Добавлен route на корзину

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<h1>Корзина</h1>} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,42 +14,44 @@ export const Header = () => {
             </div>
           </div>
         </Link>
-        <div className="header__cart">
-          <a href="/cart.html" className="button button--cart">
-            <span>520 ₽</span>
-            <div className="button__delimiter"></div>
-            <svg
-              width="18"
-              height="18"
-              viewBox="0 0 18 18"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M 6.33333 16.3333 C 7.06971 16.3333 7.66667 15.7364 7.66667 15 C 7.66667 14.2636 7.06971 13.6667 6.33333 13.6667 C 5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333 Z"
-                stroke="white"
-                strokeWidth="1.8"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M 14.3333 16.3333 C 15.0697 16.3333 15.6667 15.7364 15.6667 15 C 15.6667 14.2636 15.0697 13.6667 14.3333 13.6667 C 13.597 13.6667 13 14.2636 13 15 C 13 15.7364 13.597 16.3333 14.3333 16.3333 Z"
-                stroke="white"
-                strokeWidth="1.8"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-              <path
-                d="M 4.78002 4.99999 H 16.3334 L 15.2134 10.5933 C 15.1524 10.9003 14.9854 11.176 14.7417 11.3722 C 14.4979 11.5684 14.1929 11.6727 13.88 11.6667 H 6.83335 C 6.50781 11.6694 6.1925 11.553 5.94689 11.3393C5.70128 11.1256 5.54233 10.8295 5.50002 10.5067 L 4.48669 2.82666 C 4.44466 2.50615 4.28764 2.21182 4.04482 1.99844C3.80201 1.78505 3.48994 1.66715 3.16669 1.66666 H 1.66669"
-                stroke="white"
-                strokeWidth="1.8"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <span>3</span>
-          </a>
-        </div>
+        <Link to="/cart">
+          <div className="header__cart">
+            <a href="/cart.html" className="button button--cart">
+              <span>520 ₽</span>
+              <div className="button__delimiter"></div>
+              <svg
+                width="18"
+                height="18"
+                viewBox="0 0 18 18"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M 6.33333 16.3333 C 7.06971 16.3333 7.66667 15.7364 7.66667 15 C 7.66667 14.2636 7.06971 13.6667 6.33333 13.6667 C 5.59695 13.6667 5 14.2636 5 15C5 15.7364 5.59695 16.3333 6.33333 16.3333 Z"
+                  stroke="white"
+                  strokeWidth="1.8"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M 14.3333 16.3333 C 15.0697 16.3333 15.6667 15.7364 15.6667 15 C 15.6667 14.2636 15.0697 13.6667 14.3333 13.6667 C 13.597 13.6667 13 14.2636 13 15 C 13 15.7364 13.597 16.3333 14.3333 16.3333 Z"
+                  stroke="white"
+                  strokeWidth="1.8"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M 4.78002 4.99999 H 16.3334 L 15.2134 10.5933 C 15.1524 10.9003 14.9854 11.176 14.7417 11.3722 C 14.4979 11.5684 14.1929 11.6727 13.88 11.6667 H 6.83335 C 6.50781 11.6694 6.1925 11.553 5.94689 11.3393C5.70128 11.1256 5.54233 10.8295 5.50002 10.5067 L 4.48669 2.82666 C 4.44466 2.50615 4.28764 2.21182 4.04482 1.99844C3.80201 1.78505 3.48994 1.66715 3.16669 1.66666 H 1.66669"
+                  stroke="white"
+                  strokeWidth="1.8"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <span>3</span>
+            </a>
+          </div>
+        </Link>
       </div>
     </div>
   );
