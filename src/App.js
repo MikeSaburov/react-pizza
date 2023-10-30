865503ce096fdaf56b5a2dfc036ea7b50ce16d32
Mikhail Saburov <saburov1985@gmail.com>
2023-10-30 20:32:33 +1100

    feature(Cart.jsx): Добавлена разметка

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
+import { Cart } from './pages/Cart';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<h1>Корзина</h1>} />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
