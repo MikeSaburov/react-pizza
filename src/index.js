778ad7f4742cd1017f4582aad667bc5e7d85dd23
Mikhail Saburov <saburov1985@gmail.com>
2023-10-30 20:10:30 +1100

    fix(index.jsx): Корректно подключен Router

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-import { NotFound } from './pages/NotFound';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: 'not-found',
-    element: <NotFound />,
-  },
-]);
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
