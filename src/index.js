fe5f472f7587ad8e15ff791293ad7f0f61c86789
Mikhail Saburov <saburov1985@gmail.com>
2023-11-03 22:25:16 +1100

    feature(index.js): подключен store
    сделали store доступным для наших компонентов React, поместив React-Redux <Provider> вокруг нашего приложения в src/index.js. Импортируем только что созданный store, поместитв <Provider> вокруг вашего <App/>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -8,7 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
