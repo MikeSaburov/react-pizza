f1812d373fe1eb669e98cd22a0ab84764b1068e0
Mikhail Saburov <saburov1985@gmail.com>
2023-11-14 20:44:26 +1100

    refactor(Home): Удалил qs и navigate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,10 @@ import { PizzaBlock } from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
 import axios from 'axios';
-import qs from 'qs';
 import { useContext, useEffect, useState } from 'react';
 import { SearchContext } from '../App';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCategoryId } from '../redux/slices/filterSlice';
-import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
   const { searchValue } = useContext(SearchContext);
@@ -17,8 +15,6 @@ export const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const { categoryId, sort } = useSelector((state) => state.filter);
-  const sortType = sort.sortProperty;
-  const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
@@ -26,39 +22,22 @@ export const Home = () => {
     dispatch(setCategoryId(id));
   };
 
-  // параметры url парсим в объект
-  useEffect(() => {
-    if (window.location.search) {
-      const params = qs.parse(window.location.search.substring(1));
-      console.log(params);
-    }
-  }, []);
-
   //Запрос с backend
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-
+      const sortBy = sort.sortProperty;
       const category = categoryId > 0 ? `category=${categoryId}` : '';
       const search = searchValue ? `&name=*${searchValue}` : '';
       const response = await axios.get(
-        `https://a4b2f70c0a223b33.mokky.dev/items?${category}&sortBy=${sortType}${search}`
+        `https://a4b2f70c0a223b33.mokky.dev/items?${category}&sortBy=${sortBy}${search}`
       );
       setItems(response.data);
       setIsLoading(false);
     };
     fetchData();
     //window.scrollTo(0, 0); //Сброс скролла
-  }, [categoryId, sortType, searchValue]);
-
-  useEffect(() => {
-    const queryString = qs.stringify({
-      sortProperty: sortType,
-      categoryId,
-    });
-
-    navigate(`?${queryString}`);
-  }, [categoryId, sortType]);
+  }, [categoryId, sort.sortProperty, searchValue]);
 
   const pizzas = items.map((obj) => <PizzaBlock {...obj} key={obj.id} />);
 
