8eb299c756fe5b4f7ae00818fe9c2fcd12066980
Mikhail Saburov <saburov1985@gmail.com>
2023-11-15 21:14:28 +1100

    refactor(CartItem): добавлены пропсы

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-export const CartItem = () => {
+export const CartItem = ({ id, name, imgUrl, price, count, type }) => {
   return (
     <div className="cart__item">
       <div className="cart__item-img">
-        <img
-          className="pizza-block__image"
-          src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-          alt="Pizza"
-        />
+        <img className="pizza-block__image" src={imgUrl} alt="Pizza" />
       </div>
       <div className="cart__item-info">
-        <h3>Сырный цыпленок</h3>
-        <p>тонкое тесто, 26 см.</p>
+        <h3>{name}</h3>
+        <p>{type}, 26 см.</p>
       </div>
       <div className="cart__item-count">
         <div className="button button--outline button--circle cart__item-count-minus">
@@ -33,7 +29,7 @@ export const CartItem = () => {
             />
           </svg>
         </div>
-        <b>2</b>
+        <b>{count}</b>
         <div className="button button--outline button--circle cart__item-count-plus">
           <svg
             width="10"
@@ -54,7 +50,7 @@ export const CartItem = () => {
         </div>
       </div>
       <div className="cart__item-price">
-        <b>770 ₽</b>
+        <b>{price * count} ₽</b>
       </div>
       <div className="cart__item-remove">
         <div className="button button--outline button--circle">
