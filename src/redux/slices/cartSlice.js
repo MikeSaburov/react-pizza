1d6e07e2611e93dfdcd12e2e8f541021018c2b5f
Mikhail Saburov <saburov1985@gmail.com>
2023-11-15 21:03:55 +1100

    fix(cartSlice,Cart): реализован подсчет и удаление кода
    1. Реализован правльный подсчет всех пиц которые добавляем в корзину в cartSlice
    2. Удалены лишне карточки пицц (статичный html код) из корзины

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -27,7 +27,7 @@ const cartSlice = createSlice({
       }
 
       state.totalPrice = state.items.reduce((sum, obj) => {
-        return sum + obj.price;
+        return sum + obj.price * obj.count;
       }, 0);
     },
     removeItem(state, action) {
