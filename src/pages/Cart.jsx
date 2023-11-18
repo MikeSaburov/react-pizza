5b3afc5ed14914dec6882588af6eeb9fa1b73848
Mikhail Saburov <saburov1985@gmail.com>
2023-11-18 22:04:28 +1100

    refactor(Cart) state заменил на selectCart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { CartItem } from '../components/CartItem';
-import { clearItems } from '../redux/slices/cartSlice';
+import { clearItems, selectCart } from '../redux/slices/cartSlice';
 import { CartEmpty } from '../components/CartEmpty';
 
 export const Cart = () => {
   const dispatch = useDispatch();
-  const { totalPrice, items } = useSelector((state) => state.cart);
+  const { totalPrice, items } = useSelector(selectCart);
 
   const totalCount = items.reduce((sum, item) => {
     return sum + item.count;
