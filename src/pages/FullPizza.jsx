9d7870c0b979c344dc1b7634a6b0a466f229b045
Mikhail Saburov <saburov1985@gmail.com>
2023-11-19 22:28:19 +1100

    style(FullPizza): накинул html

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
 export const FullPizza = () => {
-  return <div>FullPizza</div>;
+  return (
+    <div>
+      <img src="" alt="Image" />
+      <h2>Маргарита</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
+        corrupti alias, delectus ex quis id atque suscipit consectetur natus
+        cum, sapiente et harum pariatur quo quas possimus sint quae tenetur.
+      </p>
+      <h4>250 ₽</h4>
+    </div>
+  );
 };
