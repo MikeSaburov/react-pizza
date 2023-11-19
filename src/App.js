e3d8961c7de9304060607614e83bb8e33f79c138
Mikhail Saburov <saburov1985@gmail.com>
2023-11-19 22:31:31 +1100

    feature(App): настроил роут для FullPizza

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import { Cart } from './pages/Cart';
-import { createContext } from 'react';
+import { FullPizza } from './pages/FullPizza';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/pizza/:id" element={<FullPizza />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
