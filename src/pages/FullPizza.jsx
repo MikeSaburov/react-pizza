d5fdcc89a5f0b6903bec419e897b6bc66f746447
Mikhail Saburov <saburov1985@gmail.com>
2023-11-19 22:36:04 +1100

    refactor(FullPizza): импортирова useParams()

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export const FullPizza = () => {
+  const params = useParams();
   return (
     <div className="container">
       <img src="" alt="Image" />
