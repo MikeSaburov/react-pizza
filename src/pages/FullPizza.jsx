c1f28c56857695e6f38195d6b7bd999a1551acd2
Mikhail Saburov <saburov1985@gmail.com>
2023-11-20 22:28:29 +1100

    refactor(FullPizza): возврат на главную если пиццы нет
    возвращаемся на главную если пиууа не найдена
    например ввели .../pizza/596 такой пиццы нет, сразу перекидываем на главную

--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export const FullPizza = () => {
   const [pizza, setPizza] = useState();
   const { id } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchPizza() {
@@ -15,6 +16,7 @@ export const FullPizza = () => {
         setPizza(data);
       } catch (error) {
         console.log('Ошибка пр получении пиццы');
+        navigate('/');
       }
     }
 
