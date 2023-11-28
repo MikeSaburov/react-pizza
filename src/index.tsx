d32ac694d8edf0ee1bf7ed06ec575daa364b89e9
Mikhail Saburov <saburov1985@gmail.com>
2023-11-28 20:24:26 +1100

    refactor(App,index): изменено расширение файлов

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './App.tsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
